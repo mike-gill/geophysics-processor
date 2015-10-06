8765db588d5cd16d5d8e38cba873141ba6703656
Mike Gill <mjg.gbr@gmail.com>
2015-10-06 17:25:23 +0100

    Added txt to xyz conversion support, allowing removal of image gradient output to be convertd to xyz for use in Snuffler

---
+++ geoconverter/txt_to_xyz.bat
@@ -0,0 +1,12 @@
+cls
+@ECHO OFF 
+REM ***********************************************
+REM * Run this Windows batch file to plot a Snuffler
+REM * XYZ file.
+REM *
+REM * Author:		Mike Gill
+REM * Copyright:	Mike Gill
+REM ***********************************************
+
+C:\Python26\python.exe src\geoconverter\TxtToXyz.py "C:\Users\Mike\archaeology\geophysics\results\townsend lane martin\townsend lane martin 20150926\EarthRes 26Sep15 11-31-24_cleaned_txt_REFORMATTED Gaussian Subtract.txt" "C:\Users\Mike\archaeology\geophysics\results\townsend lane martin\townsend lane martin 20150926\EarthRes 26Sep15 11-31-24_cleaned_txt_REFORMATTED Gaussian SubtractXYZ.txt"
+pause
